996cb0be234a8e8397a7bd392b15f37c973c04d5
san <san@gmail.com>
2025-03-31 17:46:42 +0530

    ff

--- src/threads/ProducerConsumerProblem.java
+++ src/threads/ProducerConsumerProblem.java
@@ -5,19 +5,19 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * Problem,
- * 1. Consumer threads will remove one element at a time from the bucket
- *    infinitely when bucket is not empty
- * 2. Producer threads will add an element into a bucket when no of elements in the thread is less than 10
- *
- * synchronised is a block of code that is executed only by a single thread at any given point of time
- * synchronised block need a Lock-key for any thread to enter into the block, that key should be
- * the resource that we are trying to protect.
- *
- * In the below example, We are using bucket as a lock-key for both Producer and Consumer thread.
- * Hence, among all thread we created, at any given point of time, only one thread will be inside
- * the synchronised block
- *
+  Problem,
+  1. Consumer threads will remove one element at a time from the bucket
+     infinitely when bucket is not empty
+  2. Producer threads will add an element into a bucket when no of elements in the thread is less than 10
+
+  synchronised is a block of code that is executed only by a single thread at any given point of time
+  synchronised block need a Lock-key for any thread to enter into the block, that key should be
+  the resource that we are trying to protect.
+
+  In the below example, We are using bucket as a lock-key for both Producer and Consumer thread.
+  Hence, among all thread we created, at any given point of time, only one thread will be inside
+  the synchronised block
+
  */
 public class ProducerConsumerProblem {
 
