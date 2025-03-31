d25b288fc4fd4ffe160e7f31ec1cfa19a14bba59
san <san@gmail.com>
2025-03-31 17:05:15 +0530

    ff

---
+++ src/threads/JoinThreadExample.java
@@ -0,0 +1,60 @@
+package threads;
+
+/**
+ * problem: Get data from 2 domain API parallel and process them once both
+ * APIs are returned
+ */
+public class JoinThreadExample {
+    public static void main(String[] args) {
+        API1 api1 = new API1();
+        API2 api2 = new API2();
+
+        Thread api1Thread = new Thread(api1, "API1");
+        Thread api2Thread = new Thread(api2, "API2");
+
+
+        api1Thread.start();
+        api2Thread.start();
+
+        // Thread which executes join will wait hte thread to execute before moving foreword
+        // here main thread will wait for both thread to complete before processing
+        try {
+            api1Thread.join();
+            api2Thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Processing data from API1 and API2");
+
+    }
+}
+
+/**
+ * MIMICS API1 call
+ */
+class API1 implements Runnable {
+    @Override
+    public void run() {
+        try {
+            System.out.println(" Retrieving Data from API1");
+            Thread.sleep(5);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+/**
+ * MIMICS API2 call
+ */
+class API2 implements Runnable {
+    @Override
+    public void run() {
+        try {
+            System.out.println(" Retrieving Data from API2");
+            Thread.sleep(3);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/threads/ThreadsBasics.java
+++ src/threads/ThreadsBasics.java
@@ -26,23 +26,34 @@ package threads;
 public class ThreadsBasics {
     // Starting point of main thread
     public static void main(String[] args) {
+
         EvenNumbers t1 = new EvenNumbers(); // thread state - new
         t1.start(); // thread starts after this is executed - thread state - runnable
         System.out.println("END of main thread " + "  -> " + Thread.currentThread().getName());
+
+        //-----------ODD number thread-------------
+
+        OddNumbers oddNumbers = new OddNumbers();
+        Thread oddNumberThread = new Thread(oddNumbers, "Odd Number thread");
+        Thread oddNumberThread2 = new Thread(oddNumbers, "Odd Number thread2");
+        oddNumberThread.start();
+        oddNumberThread2.start();
     }
 }
 
-// One way to create thread is to extend Thread class and implement run method.
-// Not recommend as we cannot extend any other class like Serializable or Cloneable.
+/**
+ * One way to create thread is to extend Thread class and implement run method.
+ * Not recommend as we cannot extend any other class like Serializable or Cloneable.
+ */
 class EvenNumbers extends Thread {
 
     /**
-     * starting point of thread
      * Thread.sleep has throws InterruptedException in its signature
-     * ut run method does has this, so we have to surround sleep method inside try catch
+     * But run method does not have this, so we have to surround sleep method inside try catch
      */
     @Override
-    public void run() {
+    public void run() { // starting point of thread
+        Thread.currentThread().setName("EVEN numbers thread");
         try {
             for (int i = 0; i <= 10; i = i + 2) {
                 System.out.println(i + " ->  " + Thread.currentThread().getName());
@@ -52,6 +63,19 @@ class EvenNumbers extends Thread {
 
         }
     } // thread state - DEAD
+}
+
+/**
+ * Implementing Runnable interface is recommended way to create thread
+ */
+class OddNumbers implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i=1;i<11;i=i+2) {
+            System.out.println(i + " -> " + Thread.currentThread().getName());
+        }
+    }
 }
 
 
